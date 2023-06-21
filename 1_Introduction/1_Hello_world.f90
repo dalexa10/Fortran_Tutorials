07cc028144cdd498cb8c05dc29920fbf52758b2d
Dario <dario.rodriguez9378@hotmail.com>
2023-06-21 15:40:55 -0500

    First code created

---
+++ 1_Introduction/1_Hello_world.f90
@@ -0,0 +1,8 @@
+program hello
+    ! Comment like this
+    print *, 'This is Fortran'
+end program hello
+
+!Compile the code:
+!   - Verify gfortan --version (if not, install it)
+!   - Compile this gfortran file_name.f90 -o output_compiled_file
